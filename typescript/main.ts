f4b4254a7eef3ae8e8c5be50af30c197ece03076
Jaeyeon Kim <jaeyeonling@gmail.com>
2019-02-07 20:29:16 +0900

    change like typescript

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,46 +1,43 @@
-const http = require('http')
-const url = require('url')
+import { createServer, IncomingMessage, ServerResponse, Server } from 'http'
+import { parse, UrlWithStringQuery } from 'url'
 
-const helloWorldHandler = (req, res, name) => {
-    switch (req.method) {
+const helloWorldHandler = (req: IncomingMessage, res: ServerResponse, name: string) => {
+  switch (req.method) {
     case "GET":
-        res.write(`Hello ${name}`)
-        res.end()
-        break
+      res.write(`Hello ${name}`)
+      break
     default:
-        res.statusCode = 404
-        res.end()
+      res.statusCode = 404
+      break
     }
+
+    res.end()
 }
 
-const loggingMiddleware = (req, res, next, options) => {
+const loggingMiddleware = (req: IncomingMessage, res: ServerResponse, next: (req: IncomingMessage, res: ServerResponse, name: string) => void, options: string) => {
     console.log("logging...")
     next(req, res, options)
 }
 
-const router = (req, res) => {
-    // GET /hello/{name}
-    const parsedUrl = url.parse(req.url)
-     // path: /a/b/c => ['', a, b, c]
-     // path: / => ["",""]
-    const splitedPath = parsedUrl.path.split("/")
+const router = (req: IncomingMessage, res: ServerResponse) => {
+  // GET /hello/{name}
+  const parsedUrl: UrlWithStringQuery = parse(req.url)
+  // path: /a/b/c => ['', a, b, c]
+  // path: / => ["",""]
+  const splitedPath: string[] = parsedUrl.path.split("/")
     
-    const firstDepthAction = splitedPath[1]
-    switch (firstDepthAction) {
+  const firstDepthAction: string = splitedPath[1]
+  switch (firstDepthAction) {
     case "hello":
-        const helloNameParams = splitedPath[2]
-        loggingMiddleware(req, res, helloWorldHandler, helloNameParams)
-    break
+      const helloNameParams: string = splitedPath[2]
+      loggingMiddleware(req, res, helloWorldHandler, helloNameParams)
+      break
     default:
-        res.statusCode = 404
-        res.end()
+      res.statusCode = 404
+      res.end()
+      break
     }
 }
 
-const server = http.createServer((req, res) => {
-    router(req, res)
-})
-server.on('clientError', (err, socket) => {
-    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
-})
+const server: Server = createServer(router)
 server.listen(8000)
